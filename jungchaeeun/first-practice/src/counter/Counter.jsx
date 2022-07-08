8c9c2addb9b9b0c7c109decd2fac9aab9e5fe9d0
chaeeunj <official2930@naver.com>
2022-07-09 02:59:30 +0900

    styled-components 설치 망함

--- jungchaeeun/first-practice/src/counter/Counter.jsx
+++ jungchaeeun/first-practice/src/counter/Counter.jsx
@@ -1,24 +1,23 @@
 import React, { useState } from 'react';
+import styled from 'styled-components;';
 
-const styles = {
-  main: {
-    margin: '100px',
-    alignItems: 'center',
-    border: '3px solid blue',
-    display: 'flex',
-    justifyContent: 'center',
-    width: '600px',
-    height: '400px',
-  },
+const Main = styled.main`
+    margin: 100px,
+    alignItems: center,
+    border: 3px solid blue,
+    display: flex,
+    justifyContent: center,
+    width: 600px,
+    height: 400px,
+`;
 
-  button: {
-    margin: '50px',
-    width: '40px',
-    height: '40px',
-    backgroundColor: 'lightgresy',
-    border: 'none',
-  },
-};
+const Button = styled.Button`
+    margin: 50px,
+    width: 40px,
+    height: 40px,
+    backgroundColor: lightgresy,
+    border: none,
+`;
 
 const Counter = () => {
   const [count, setCount] = useState(0);
@@ -40,18 +39,18 @@ const Counter = () => {
   };
 
   return (
-    <main style={styles.main}>
-      <button style={styles.button} onClick={onDecreaseTwo}>
+    <main style={Main}>
+      <button style={Button} onClick={onDecreaseTwo}>
         -2
       </button>
-      <button style={styles.button} onClick={onDecreaseOne}>
+      <button style={Button} onClick={onDecreaseOne}>
         -1
       </button>
       <h1>{count}</h1>
-      <button style={styles.button} onClick={onIncreaseOne}>
+      <button style={Button} onClick={onIncreaseOne}>
         +1
       </button>
-      <button style={styles.button} onClick={onIncreaseTwo}>
+      <button style={Button} onClick={onIncreaseTwo}>
         +2
       </button>
     </main>
