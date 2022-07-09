b931e0db52cec2d9b78310572309ed18089832fa
chaeeunj <official2930@naver.com>
2022-07-09 20:25:01 +0900

    refactor: styled-components 적용

--- jungchaeeun/first-practice/src/counter/Counter.jsx
+++ jungchaeeun/first-practice/src/counter/Counter.jsx
@@ -1,23 +1,5 @@
 import React, { useState } from 'react';
-import styled from 'styled-components;';
-
-const Main = styled.main`
-    margin: 100px,
-    alignItems: center,
-    border: 3px solid blue,
-    display: flex,
-    justifyContent: center,
-    width: 600px,
-    height: 400px,
-`;
-
-const Button = styled.Button`
-    margin: 50px,
-    width: 40px,
-    height: 40px,
-    backgroundColor: lightgresy,
-    border: none,
-`;
+import styled from 'styled-components';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
@@ -39,22 +21,42 @@ const Counter = () => {
   };
 
   return (
-    <main style={Main}>
-      <button style={Button} onClick={onDecreaseTwo}>
-        -2
-      </button>
-      <button style={Button} onClick={onDecreaseOne}>
-        -1
-      </button>
-      <h1>{count}</h1>
-      <button style={Button} onClick={onIncreaseOne}>
-        +1
-      </button>
-      <button style={Button} onClick={onIncreaseTwo}>
-        +2
-      </button>
-    </main>
+    <Wrapper>
+      <Main>
+        <Button onClick={onDecreaseTwo}>-2</Button>
+        <Button onClick={onDecreaseOne}>-1</Button>
+        <h1>{count}</h1>
+        <Button onClick={onIncreaseOne}>+1</Button>
+        <Button onClick={onIncreaseTwo}>+2</Button>
+      </Main>
+    </Wrapper>
   );
 };
 
 export default Counter;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Main = styled.div`
+  margin: 100px;
+  align-items: center;
+  border: 3px solid blue;
+  display: flex;
+  justify-content: center;
+  width: 600px;
+  height: 400px;
+`;
+
+const Button = styled.button`
+  margin: 50px;
+  width: 40px;
+  height: 40px;
+  background-color: lightgrey;
+  border: none;
+`;
