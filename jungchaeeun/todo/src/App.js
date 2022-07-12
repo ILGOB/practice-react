3b1616478b75634db34633f2414e7a368531cb7c
chaeeunj <official2930@naver.com>
2022-07-12 21:16:32 +0900

    Refactor: input 창과 button 이름 변경

--- jungchaeeun/todo/src/App.js
+++ jungchaeeun/todo/src/App.js
@@ -2,11 +2,17 @@ import { useState } from 'react';
 
 function App() {
   const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log(toDo);
+    if (toDo === '') {
+      return;
+    }
+    setToDo('');
+    setToDos((currentArray) => [toDo, ...currentArray]);
   };
+  console.log(toDos);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -14,10 +20,11 @@ function App() {
           onChange={onChange}
           value={toDo}
           type="text"
-          placeholder="Write your to do..."
+          placeholder="할 일을 입력하세요"
         />
-        <button>Add To Do</button>
+        <button>추가</button>
       </form>
+      <h1>Todo List</h1>
     </div>
   );
 }
